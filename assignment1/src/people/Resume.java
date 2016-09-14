b611affe676dc70667311e782907c0e682e1aee9
Fan Junxi <fan.ju@husky.neu.edu>
2016-09-14 14:14:25 -0400

    create flies in Bitbucket

---
+++ assignment1/src/people/Resume.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package people;
+
+/**
+ *
+ * @author fjx19
+ */
+public class Resume {
+    
+    private String firstname;
+    private String lastname;
+    
+   
+   
+    //图片
+    private String dateofbirth;
+    
+    private String age;
+    
+    private String Tel;
+    
+    private String email;
+     
+    private String streetln1;
+    private String streetln2;
+    private String city;
+    private String country;
+    
+    private String affiliation;
+    
+    private String major;
+    
+    private String career;
+   
+    private String degree1;
+    private String degree1start;
+    private String degree1end;
+    private String degree2;
+    private String degree2start;
+    private String degree2end;
+    
+   
+    
+    
+}
